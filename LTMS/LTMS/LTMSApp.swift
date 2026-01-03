9a80ed77140456988691abd58b770b8b2beafb1f
shubham singh <shubham2450.be23@chitkara.edu.in>
2026-01-03 19:28:48 +0530

    Project

---
+++ LTMS/LTMS/LTMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LTMSApp.swift
+//  LTMS
+//
+//  Created by Shubham Singh on 03/01/26.
+//
+
+import SwiftUI
+
+@main
+struct LTMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
